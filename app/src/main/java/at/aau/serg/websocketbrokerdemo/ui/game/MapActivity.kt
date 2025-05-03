3ae5067ac92c839e84ebe75d348d4e898cfb06c7
markusmessneraau <115398958+markusmessneraau@users.noreply.github.com>
2025-05-03 11:28:31 +0200

    Fix:Restore MapActivity.kt

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/MapActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/MapActivity.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
