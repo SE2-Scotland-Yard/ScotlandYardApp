0c04a2c9af710987c8bd6dff56c417e878a19784
eliasra <eliasra@edu.aau.at>
2025-05-19 09:23:11 +0200

    added simple SideBar

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/GameScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/GameScreen.kt
@@ -33,17 +33,59 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
+
 
 import com.example.myapplication.R
 
 
 @Composable
-fun GameScreen(){
+fun GameScreen(
+    gameId: String,
+    lobbyVm: LobbyViewModel,
+    userSessionVm: UserSessionViewModel,
+    gameVm: GameViewModel
+) {
+    val username = userSessionVm.username.value
+    val gameUpdate by lobbyVm.gameState.collectAsState()
+    val message by remember { derivedStateOf { gameVm.message } }
+    val allowedMoves by remember { derivedStateOf { gameVm.allowedMoves } }
+    val mrXPosition by remember { derivedStateOf { gameVm.mrXPosition }}
+    val allowedMovesDetails by remember { derivedStateOf { gameVm.allowedMovesDetails }}
+    val error by remember { derivedStateOf { gameVm.errorMessage } }
 
+    var expanded by remember { mutableStateOf(false) }
+    var selectedMove by remember { mutableStateOf<Int?>(null) }
+
+    val isMyTurn = username == gameUpdate?.currentPlayer
+
+    // Moves nach dem Join laden
+    LaunchedEffect(gameId, username) {
+        if (username != null) {
+            gameVm.fetchAllowedMoves(gameId, username)
+            gameVm.fetchMrXPosition(gameId,username)
+        }
+    }
+
+    LaunchedEffect(gameUpdate?.currentPlayer) {
+        if (username != null && gameUpdate?.currentPlayer == username) {
+            gameVm.fetchAllowedMoves(gameId, username)
+            gameVm.fetchMrXPosition(gameId,username)
+        }
+    }
 }
 
 @Composable
-fun Map(useSmallMap: Boolean = false){
+fun Map(
+    gameId: String,
+    lobbyVm: LobbyViewModel,
+    userSessionVm: UserSessionViewModel,
+    gameVm: GameViewModel,
+    useSmallMap: Boolean = false
+){
     var scale by remember { mutableFloatStateOf(1f) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableFloatStateOf(0f) }
@@ -100,6 +142,79 @@ fun Map(useSmallMap: Boolean = false){
     }
 }
 
-fun MainBar(){
+@Composable
+fun BottomBar(
+    gameId: String,
+    lobbyVm: LobbyViewModel,
+    userSessionVm: UserSessionViewModel,
+    gameVm: GameViewModel
+){
 
+}
+
+@Composable
+fun SideBar(
+    gameId: String,
+    lobbyVm: LobbyViewModel,
+    userSessionVm: UserSessionViewModel,
+    gameVm: GameViewModel
+){
+    val username = userSessionVm.username.value
+    val gameUpdate by lobbyVm.gameState.collectAsState()
+    val message by remember { derivedStateOf { gameVm.message } }
+    val allowedMoves by remember { derivedStateOf { gameVm.allowedMoves } }
+    val mrXPosition by remember { derivedStateOf { gameVm.mrXPosition }}
+    val allowedMovesDetails by remember { derivedStateOf { gameVm.allowedMovesDetails }}
+    val error by remember { derivedStateOf { gameVm.errorMessage } }
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedMove by remember { mutableStateOf<Int?>(null) }
+
+    val isMyTurn = username == gameUpdate?.currentPlayer
+
+    Column(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+    ) {
+        username?.let {
+            Text("Eingeloggt als: $it", style = MaterialTheme.typography.titleMedium)
+            Spacer(Modifier.height(16.dp))
+        }
+        userSessionVm.role.value?.let {
+            Text("Du bist: $it", style = MaterialTheme.typography.titleMedium)
+            Spacer(Modifier.height(16.dp))
+        }
+
+        Text("Spielerpositionen:", style = MaterialTheme.typography.titleLarge)
+        Spacer(Modifier.height(8.dp))
+
+
+        if (userSessionVm.role.value=="MRX") {
+            mrXPosition?.let {
+                Text("MrX steht auf: $it")
+            }
+            Spacer(Modifier.height(16.dp))
+            gameUpdate?.playerPositions?.forEach { (name, pos) ->
+                Text("$name steht auf Feld $pos")
+            }
+        }else {
+            gameUpdate?.playerPositions?.forEach { (name, pos) ->
+                Text("$name steht auf Feld $pos")
+            }
+        }
+        Spacer(Modifier.height(4.dp))
+        val myPosition = gameUpdate?.playerPositions?.get(username)
+        myPosition?.let {
+            Spacer(Modifier.height(16.dp))
+            Text("➡ Du stehst auf Feld $it", style = MaterialTheme.typography.titleMedium)
+            Spacer(Modifier.height(16.dp))
+        }
+
+        // Hier wird die Nachricht angezeigt
+        if (message.isNotEmpty()) {
+            Spacer(Modifier.height(16.dp))
+            Text(message, color = MaterialTheme.colorScheme.primary)
+        }
+    }
 }
