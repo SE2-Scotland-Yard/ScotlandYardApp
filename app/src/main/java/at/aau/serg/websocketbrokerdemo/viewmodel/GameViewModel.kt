4d246a49e5ae6ea7d65c3436341fbdb1b1d964b7
noxar007 <manuel.stingl@gmx.at>
2025-05-29 01:08:51 +0200

    changed var in method move to blackmove

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/viewmodel/GameViewModel.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/viewmodel/GameViewModel.kt
@@ -54,8 +54,8 @@ class GameViewModel(
     fun move(gameId: String, name: String, to: Int, gotTicket: String) {
        viewModelScope.launch {
            try {
-               var blackTicket = "BLACK"
-               message = repository.move(gameId, name, to, blackTicket)
+
+               message = repository.move(gameId, name, to, gotTicket)
            }catch (e:Exception){
                errorMessage = e.message
            }
@@ -65,7 +65,8 @@ class GameViewModel(
     fun blackMove(gameId: String, name: String, to: Int, gotTicket: String) {
         viewModelScope.launch {
             try {
-                message = repository.blackMove(gameId, name, to, gotTicket)
+                var blackTicket = "BLACK"
+                message = repository.blackMove(gameId, name, to, blackTicket)
             }catch (e:Exception){
                 errorMessage = e.message
             }
